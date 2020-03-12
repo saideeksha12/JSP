e27f28a794f07ed4acd031f8d8be7c9b4f83da73
DELL <DELL@home>
2020-03-12 13:06:12 +0530

    hiii

--- collectionexamples/src/AgeComparator.java
+++ collectionexamples/src/AgeComparator.java
@@ -6,7 +6,7 @@ public class AgeComparator implements Comparator {
 	public int compare(Object o1, Object o2) {
 		Student1 s1 = (Student1) o1;
 		Student1 s2 = (Student1) o2;
-		System.out.println("sree");
+		System.out.println("malli");
 		if (s1.age == s2.age)
 			return 0;
 		else if (s1.age > s2.age)
